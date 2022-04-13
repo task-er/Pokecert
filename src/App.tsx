247bea3fde9b3e1acafdc6e1bd012b0f3340a0e8
task-er <task.complex@gmail.com>
2022-04-14 00:44:23 +0900

    feat: 간단한 프로젝트이기 떄문에 사용할 구조만 남기고 정리

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,13 @@
 import React, { ReactElement } from 'react'
-import logo from './logo.svg'
+import pokeball from './pokeball.png'
 import '@dir/App.css'
 
 const App = (): ReactElement => {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img src={pokeball} className="App-logo" alt="logo" />
+        <p>Pokecert.</p>
       </header>
     </div>
   )
