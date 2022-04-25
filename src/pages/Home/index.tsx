20037f47cb73ed8d0579beca191884421af2f9c4
task-er <task.complex@gmail.com>
2022-04-25 22:21:45 +0900

    6 feat: Home 화면 반응형 sample 적용

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -8,8 +8,8 @@ const Home = (): ReactElement => {
   return (
     <div className="home-wrapper">
       <div className="home-layout">
-        <img src={mu} alt="character1" />
-        <img src={mu} alt="character1" />
+        <img src={mu} alt="character1" className="main-character" />
+        <img src={mu} alt="character1" className="main-character" />
 
         <Button page="/list" value="띠부띠부씰 종류 보기" />
         <Button page="/Collection" value="내가 가진 띠부띠부씰" />
