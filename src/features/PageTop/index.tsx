04f0c86a44942aa90959ddbdfe66499c2bd992d8
task-er <task.complex@gmail.com>
2022-05-30 18:22:53 +0900

    15 feat: 씰 다 모은 경우 좌상단에 다운로드 페이지로 이동하는 버튼 추가

--- src/features/PageTop/index.tsx
+++ src/features/PageTop/index.tsx
@@ -3,12 +3,18 @@ import { useNavigate } from 'react-router-dom'
 import PageTitle from '@components/PageTitle'
 import './index.scss'
 import SearchBox from '@components/SearchBox'
+import { useAppSelector } from '@redux/config'
 
 interface PageTopProps {
   title: string
 }
 const PageTop = ({ title }: PageTopProps): ReactElement => {
   const navigate = useNavigate()
+  const { isComplete } = useAppSelector((state) => state.checkIsCompleteSlice)
+
+  const moveToComplete = () => {
+    navigate('/complete')
+  }
 
   const moveToHome = () => {
     navigate('/')
@@ -16,6 +22,11 @@ const PageTop = ({ title }: PageTopProps): ReactElement => {
 
   return (
     <div className="page-top-layout">
+      {isComplete && (
+        <button className="move-complete-button" onClick={moveToComplete}>
+          다 모았다!
+        </button>
+      )}
       <PageTitle title={title} />
       <button onClick={moveToHome}>◀ 이전</button>
       <SearchBox />
